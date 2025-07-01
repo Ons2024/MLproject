90675d37ce2f37a25a1bd6be827800bf0615fa31
Ons Ouali <onswali28@gmail.com>
2025-07-01 21:25:13 +0200

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,28 @@
+## Ce code permet de créer une erreur personnalisée qui t’indique dans quel fichier 
+## et à quelle ligne ton erreur s’est produite, avec un message clair.
+
+
+import sys
+import logging
+
+def error_message_details(error,error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = f"Error occurred in script: [{file_name}] at line number: [{line_number}] with message: [{str(error)}]"
+    return error_message
+    
+
+class CustomException(Exception):
+    def __init__(self, error, error_detail: sys):
+        super().__init__(error)
+        self.error_message = error_message_details(error, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+
+    def __repr__(self):
+        return CustomException.__name__.str() + ": " + self.error_message
+    
+
+    
